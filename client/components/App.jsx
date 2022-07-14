93730b5cef981738d4f363dbda25622f8e51941d
erinlizzierose <erin.lizzie.rose@gmail.com>
2022-07-14 23:15:11 +1200

    practice rendering planets

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,24 @@
 import React, { Suspense } from 'react'
 import { Canvas, useLoader, useThree } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
-function Scene() {
-  const sunMap = useLoader(TextureLoader, '/images/sun.png')
+function SolarSystem() {
+  const sunMap = useLoader(TextureLoader, '/images/sun.jpg')
+  const earthMap = useLoader(TextureLoader, '/images/earth.jpg')
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity ={0.5}/>
+      <pointLight intensity={1} position ={[0,0,0]}/>
+
       <mesh>
-        <sphereGeometry args={[2]} />
+        <sphereGeometry args={[1]} />
         <meshStandardMaterial map={sunMap} />
+      </mesh>
+
+      <mesh position={[6, 0, 0]}>
+        <sphereGeometry args={[0.5]} />
+        <meshStandardMaterial map ={earthMap} />
       </mesh>
     </>
   )
@@ -17,11 +26,15 @@ function Scene() {
 
 function App() {
   return (
-    <Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+    <Canvas  camera={{ position: [0, 20, 25], fov: 15 }}>
+    <color attach="background" args={[0x000000]} />
       <Suspense fallback={null}>
-        <Scene />
+        <SolarSystem />
       </Suspense>
+      <OrbitControls/>
     </Canvas>
+    </div>
   )
 
   //insert react router
@@ -29,23 +42,3 @@ function App() {
 
 export default App
 
-// const [fruits, setFruits] = useState([])
-// useEffect(() => {
-//   getFruits()
-//   .then(fruits => {
-//     setFruits(fruits)
-//   })
-// }, [])
-
-// return (
-//   <>
-//     <div className='app'>
-//       <h1>Fullstack Boilerplate - with Fruits!</h1>
-//       <ul>
-//         {fruits.map(fruit => (
-//           <li key={fruit}>{fruit}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   </>
-// )
