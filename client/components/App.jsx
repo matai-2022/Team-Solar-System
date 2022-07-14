3c6d6480f80ad50d9eff5659b1c2854b995c18c4
erinlizzierose <erin.lizzie.rose@gmail.com>
2022-07-14 23:30:50 +1200

    added stars

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Suspense } from 'react'
-import { Canvas, useLoader, useThree } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { OrbitControls, Stars } from '@react-three/drei'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 function SolarSystem() {
   const sunMap = useLoader(TextureLoader, '/images/sun.jpg')
   const earthMap = useLoader(TextureLoader, '/images/earth.jpg')
+  
+
   return (
     <>
       <ambientLight intensity ={0.5}/>
@@ -33,6 +35,16 @@ function App() {
         <SolarSystem />
       </Suspense>
       <OrbitControls/>
+      
+      <Stars
+        radius={100} // Radius of the inner sphere (default=100)
+        depth={50} // Depth of area where stars should fit (default=50)
+        count={99000} // Amount of stars (default=5000)
+        factor={4} // Size factor (default=4)
+        saturation={0} // Saturation 0-1 (default=0)
+        fade speed={2} // Faded dots (default=false)
+      />
+
     </Canvas>
     </div>
   )
