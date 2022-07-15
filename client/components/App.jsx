8bfe88a77a56c4b6456899ebadd1bfdbff3ad29b
Lucas <luke.norris22@gmail.com>
2022-07-15 16:35:40 +1200

    unfinished pause button in app component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,17 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
 
 import SolarSystem from './SolarSystem'
 
 function App() {
+  const animate = useRef(true)
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 20, 25], fov: 15 }}>
         <color attach="background" args={[0x000000]} />
         <Suspense fallback={null}>
-          <SolarSystem />
+          <SolarSystem animate={animate} />
         </Suspense>
         <OrbitControls />
 
@@ -24,6 +25,9 @@ function App() {
           speed={1} // Faded dots (default=false)
         />
       </Canvas>
+      <button onClick={() => (animate.current = !animate.current)}>
+        Pause
+      </button>
     </div>
   )
 
