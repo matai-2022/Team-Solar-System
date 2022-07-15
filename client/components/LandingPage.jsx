7c2cf6cf213e71726f5a2e55272f99c4a0d018cb
Erin <erin.lizzie.rose@gmail.com>
2022-07-16 11:07:05 +1200

    Landing page styled- button doesnt work yet

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -13,15 +13,19 @@ const LandingPage = () => {
     >
       {/* //holds all the stuff */}
       <div className="flex flex-col h-3/4 items-center">
-        <h1 className=" flex text-9xl text-white items-center pt-10 font-sans tracking-wider">
+        <h1 className=" flex flex-col text-9xl text-white items-center pt-10 font-sans tracking-wider pb-0 mb-0">
           EXPLORE THE SOLAR SYSTEM
+          <p className="text-white text-3xl font-sans py-0 mt-0">
+            ITS ALMOST LAUNCH TIME - ARE YOU READY?? 3, 2, 1....
+          </p>
         </h1>
+
         <button
-          className="text-white items-center justify-end border-2 border-double border-white p-2"
+          className="flex text-white items-center justify-self-end border border-double border-white p-3 px-6 mt-5"
           type="button"
           onClick="onClick"
         >
-          TAKE ME AWAY
+          BLAST OFF!!!
         </button>
       </div>
     </div>
