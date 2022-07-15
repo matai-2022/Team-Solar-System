64798f2781f0206b3575c390e0b82b93939eb408
Erin <erin.lizzie.rose@gmail.com>
2022-07-15 16:58:11 +1200

    create button

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -12,10 +12,17 @@ const LandingPage = () => {
       }}
     >
       {/* //holds all the stuff */}
-      <div className="flex flex-col h-3/4 items-center ">
+      <div className="flex flex-col h-3/4 items-center">
         <h1 className=" flex text-9xl text-white items-center pt-10 font-sans tracking-wider">
           EXPLORE THE SOLAR SYSTEM
         </h1>
+        <button
+          className="text-white items-center justify-end border-2 border-double border-white p-2"
+          type="button"
+          onClick="onClick"
+        >
+          TAKE ME AWAY
+        </button>
       </div>
     </div>
   )
