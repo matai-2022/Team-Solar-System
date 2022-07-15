b46d63475d7c60f059d9db20852e7bdb5b158edc
Lucas <luke.norris22@gmail.com>
2022-07-15 14:02:52 +1200

    earth rotates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, useRef } from 'react'
 import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { DoubleSide } from 'three'
 
 function SolarSystem() {
   const sunMap = useLoader(TextureLoader, '/images/sun.jpg')
@@ -14,6 +15,16 @@ function SolarSystem() {
   const uranusMap = useLoader(TextureLoader, '/images/uranus.jpg')
   const neptuneMap = useLoader(TextureLoader, '/images/neptune.jpg')
 
+  const earthMesh = useRef()
+  const saturnMesh = useRef()
+  const ringMesh = useRef()
+
+  useFrame(({ clock }) => {
+    earthMesh.current.rotation.y += 0.01
+    saturnMesh.current.rotation.y = clock.getElapsedTime()
+    ringMesh.current.rotation.x = 1
+  })
+
   return (
     <>
       <ambientLight intensity={0.5} />
@@ -24,47 +35,51 @@ function SolarSystem() {
         <meshStandardMaterial map={sunMap} />
       </mesh>
 
-      <mesh position={[1, 0, 0]}>
-        <sphereGeometry args={[0.004]} />
+      <mesh position={[4, 0, 0]}>
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={mercuryMap} />
       </mesh>
 
-      <mesh position={[2, 0, 0]}>
-        <sphereGeometry args={[0.009]} />
+      <mesh position={[6, 0, 0]}>
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={venusMap} />
       </mesh>
 
-      <mesh position={[10, 0, 0]}>
-        <sphereGeometry args={[0.0095]} />
+      <mesh ref={earthMesh} position={[10, 0, 0]}>
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={earthMap} />
-        <mesh position={[0.2, 0, 0]}>
-          <sphereGeometry args={[0.002]} />
+        <mesh position={[1, 0, 0]}>
+          <sphereGeometry args={[0.1]} />
           <meshStandardMaterial color="#78D481" />
         </mesh>
       </mesh>
 
       <mesh position={[13, 0, 0]}>
-        <sphereGeometry args={[0.005]} />
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={marsMap} />
       </mesh>
 
       <mesh position={[15, 0, 0]}>
-        <sphereGeometry args={[0.1]} />
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={jupiterMap} />
       </mesh>
 
-      <mesh position={[18, 0, 0]}>
-        <sphereGeometry args={[0.0088]} />
+      <mesh ref={saturnMesh} position={[18, 0, 0]}>
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={saturnMap} />
+        <mesh ref={ringMesh} position={[0, 0, 0]} angle={0.15}>
+          <ringGeometry args={[0.6, 1.03, 32]} angle={0.15} />
+          <meshStandardMaterial color="#78D481" side={DoubleSide} />
+        </mesh>
       </mesh>
 
       <mesh position={[20, 0, 0]}>
-        <sphereGeometry args={[0.0037]} />
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={uranusMap} />
       </mesh>
 
       <mesh position={[22, 0, 0]}>
-        <sphereGeometry args={[0.0036]} />
+        <sphereGeometry args={[0.3]} />
         <meshStandardMaterial map={neptuneMap} />
       </mesh>
     </>
