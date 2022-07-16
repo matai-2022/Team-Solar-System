2c918e1dc25dbe7a1542477a414df86b5d4af53d
d-ashby <dashby91@gmail.com>
2022-07-16 21:08:59 +1200

    practice

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React, { Suspense } from 'react'
 import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stars } from '@react-three/drei'
 
 function Scene() {
   const sunMap = useLoader(TextureLoader, '/images/sun.png')
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={0.5} />
       <mesh>
-        <sphereGeometry args={[2]} />
+        <sphereGeometry args={[2, 15, 15]} />
         <meshStandardMaterial map={sunMap} />
       </mesh>
     </>
@@ -21,6 +21,8 @@ function App() {
     <Canvas>
       <Suspense fallback={null}>
         <OrbitControls />
+        <Stars />
+      
         <Scene />
       </Suspense>
     </Canvas>
