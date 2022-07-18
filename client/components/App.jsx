7e8bcd77abf1aa00207a78b8f87f9bb2725d0d72
Lucas Norris <luke.norris22@gmail.com>
2022-07-18 14:18:58 +1200

    chnaged to original size solar system

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-//import SolarSystem from './SolarSystem'
+import SolarSystem from './SolarSystem'
 import LandingPage from './LandingPage'
-import SolarSystemDistance from './SolarSystemDistance'
+//import SolarSystemDistance from './SolarSystemDistance'
 
 function App() {
   return (
     <div className="h-screen">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/solarsystem" element={<SolarSystemDistance />} />
+        <Route path="/solarsystem" element={<SolarSystem />} />
       </Routes>
     </div>
   )
