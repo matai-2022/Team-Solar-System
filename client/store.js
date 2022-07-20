9846f053771918e134a8a5b3203f096a4b224ac1
Erin <erin.lizzie.rose@gmail.com>
2022-07-20 16:59:47 +1200

    Four tests

--- client/store.js
+++ client/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import pause from './slices/pause'
 import planet from './slices/planet'
 
-const store = configureStore({ reducer: { pause, planet } })
+export const reducers = { pause, planet }
+
+const store = configureStore({ reducer: reducers })
 
 export default store
