838ef55aa7eaa97724a849cf27e4b9a57ed7cb3e
Lucas Norris <luke.norris22@gmail.com>
2022-07-20 15:08:42 +1200

    fix stars

--- client/components/SolarSystemDistance.jsx
+++ client/components/SolarSystemDistance.jsx
@@ -371,7 +371,7 @@ export default function SolarSystem() {
             position: [1200, 600, 50],
             fov: 50,
             near: 0.1,
-            far: 100000,
+            far: 10000000,
           }}
         >
           <color attach="background" args={[0x000000]} />
@@ -382,9 +382,9 @@ export default function SolarSystem() {
             <OrbitControls />
 
             <Stars
-              radius={50000} // Radius of the inner sphere (default=100)
-              depth={50} // Depth of area where stars should fit (default=50)
-              count={500000} // Amount of stars (default=5000)
+              radius={100000} // Radius of the inner sphere (default=100)
+              depth={5000} // Depth of area where stars should fit (default=50)
+              count={200000} // Amount of stars (default=5000)
               factor={4} // Size factor (default=4)
               saturation={0} // Saturation 0-1 (default=0)
               fade
